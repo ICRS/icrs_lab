26d3d3456c895b455392a4c1c3eee4e9959feaf2
Etienne Naude <eti@naude.dev>
2024-11-11 19:45:15 +0000

    Update all submodules

---
+++ update.sh
@@ -0,0 +1,17 @@
+cd icrs_backend;
+git switch main; git pull; cd ..;
+
+cd icrs_bot;
+git switch main; git pull; cd ..;
+
+cd icrs_card_reader;
+git switch main; git pull; cd ..;
+
+cd icrs_database;
+git switch main; git pull; cd ..;
+
+cd icrs_printer_release_button;
+git switch main; git pull; cd ..;
+
+git add .;
+git commit -m "Update all submodules";
